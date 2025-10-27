9a34ed451cf80a9b78c1e6cddcdd8ef56cae24f7
Rui Zhu <rui.zhu@abel.ai>
2025-10-27 14:32:15 +0800

    remove: get crypto list

--- src/server.py
+++ src/server.py
@@ -79,7 +79,10 @@ mcp.tool()(get_market_hours)
 mcp.tool()(get_commodities_list)
 mcp.tool()(get_commodities_prices)
 mcp.tool()(get_historical_price_eod_light)
-mcp.tool()(get_crypto_list)
+
+# remove this as the result is too long
+# mcp.tool()(get_crypto_list)
+
 mcp.tool()(get_crypto_quote)
 mcp.tool()(get_forex_list)
 mcp.tool()(get_forex_quotes)
